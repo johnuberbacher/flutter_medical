aa160baf0025b41f041aff2001efb4f3f3d142b9
John Uberbacher <johnuberbacher@gmail.com>
2020-09-30 16:11:02 -0600

    flutter_native_splash package added to generate native splashscreens

--- android/app/src/main/kotlin/com/johnuberbacher/flutter_medical/MainActivity.kt
+++ android/app/src/main/kotlin/com/johnuberbacher/flutter_medical/MainActivity.kt
@@ -2,5 +2,8 @@ package com.johnuberbacher.flutter_medical
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
